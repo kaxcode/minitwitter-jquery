bf668826b02d7a87a368a153970c7b32bfb36ac5
Kenia Valladarez <keniavalladarez@gmail.com>
2016-12-21 08:38:52 -0500

    Added a if/else js function to change the text in the button from hide icons to show icons and vice versa

--- app/assets/javascripts/tweets-index.js
+++ app/assets/javascripts/tweets-index.js
@@ -1,5 +1,18 @@
+// $(document).ready(function() {
+//   $('.toggle-icons').click(function(event) {
+//     $('.icon-box').toggle()
+//   })
+// })
+
 $(document).ready(function() {
   $('.toggle-icons').click(function(event) {
-    $('.icon-box').toggle()
+    if ($(this).text() === 'Show Icons') {
+          $('.icon-box').toggle()
+          $(this).text('Hide Icons')
+      }
+      else {
+          $('.icon-box').toggle()
+          $(this).text('Show Icons')
+      }
   })
 })
